9a374a124f084b9515776d3f92da7a2454c5f371
Dejan Zdravkovski <dejan0zdravkovski@outlook.com>
2024-09-09 00:08:14 +0200

    Last two screens

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     useEffect(() => {
         setTimeout(() => {
             setUncoolFriend(false);
+            setAngelFriend(false);
+            setPrize(false);
             setLoading(false);
         }, 3000); // Simulate a 3-second delay
     }, []);
@@ -26,6 +28,8 @@ function App() {
     const [randomFriend, setRandomFriend] = useState('');
     const [showFriendsInput, setShowFriendsInput] = useState(true);
     const [showUncoolFriend, setUncoolFriend] = useState(true);
+    const [showAngelFriend, setAngelFriend] = useState(true);
+    const [showPrize, setPrize] = useState(true);
 
     const pickRandomFriend = () => {
         const nonEmptyFriends = friends.filter(friend => friend.trim() !== '');
@@ -38,6 +42,8 @@ function App() {
 
     function loopRandomFriend(counter, nonEmptyFriends) {
         if (counter === 0) {
+            setUncoolFriend(false); // Hide the uncool friend div
+            setAngelFriend(true); // Show the angel friend div
             return;
         }
         const randomIndex = Math.floor(Math.random() * nonEmptyFriends.length);
@@ -47,6 +53,17 @@ function App() {
             counter--;
             loopRandomFriend(counter, nonEmptyFriends);
         }, 500);
+    }
+
+    const getPrize = () => {
+        setAngelFriend(false);
+        setPrize(true);
+    }
+
+    const sendEmailAddress = () => {
+        //send it to Azure Function
+
+        window.location.reload(false);
     }
 
     if (loading) {
@@ -68,12 +85,15 @@ function App() {
                 <div className="row people rounded-top text-center">
                     <div className="col-12">
                         <div className="row">
-                            <div className="col-12">
-                                <p>Input the names of everyone of the group going out tonight</p>
+                            <div className="col-1">&nbsp;</div>
+                            <div className="col-10">
+                                <p>Input the names of everyone of<br />the group going out tonight</p>
                             </div>
+                            <div className="col-1">&nbsp;</div>
                         </div>
                         <div className="row">
-                            <div className="col-12" id="uncool-friend-div">
+                            <div className="col-1">&nbsp;</div>
+                            <div className="col-10" id="uncool-friend-div">
                                 {friends.map((friend, index) => (
                                     <input
                                         key={index}
@@ -85,9 +105,11 @@ function App() {
                                     />
                                 ))}
                             </div>
+                            <div className="col-1">&nbsp;</div>
                         </div>
                         <div className="row with-margin">
-                            <div className="col-12">
+                            <div className="col-1">&nbsp;</div>
+                            <div className="col-10">
                                 <button 
                                     id="add-uncool-friend" 
                                     className="btn btn-primary uncool-friend-add-btn"
@@ -95,6 +117,7 @@ function App() {
                                         + Add friend
                                 </button>
                             </div>
+                            <div className="col-1">&nbsp;</div>
                         </div>
                         <div className="row with-margin">
                             <div className="col-12">
@@ -114,14 +137,18 @@ function App() {
                 <div className="row uncool rounded-top text-center">
                     <div className="col-12">
                         <div className="row">
-                            <div className="col-12 uncool-friend-heading">
-                                <p>CHOOSING THE UNCOOL FRIEND</p>
+                            <div className="col-1">&nbsp;</div>
+                            <div className="col-10 uncool-friend-heading">
+                                <p>CHOOSING <br />THE UNCOOL FRIEND</p>
                             </div>
+                            <div className="col-1">&nbsp;</div>
                         </div>
                         <div className="row">
-                            <div className="col-12">
-                                <p>the designated guardian angel for the whole group tonight</p>
+                            <div className="col-1">&nbsp;</div>
+                            <div className="col-10">
+                                <p>the designated guardian angel<br />for the whole group tonight:</p>
                             </div>
+                            <div className="col-1">&nbsp;</div>
                         </div>
                         {randomFriend && (
                             <div className="row with-margin">
@@ -132,6 +159,92 @@ function App() {
                                 <div className="col-3">&nbsp;</div>
                             </div>
                         )}
+                    </div>
+                </div>
+            )}
+            {showAngelFriend && (
+                <div className="row angel rounded-top text-center">
+                    <div className="col-12">
+                        <div className="row">
+                            <div className="col-12 angel-friend-heading-1">
+                                <p className="with-margin">Congratulations</p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12 angel-friend-heading-2">
+                                <p>{randomFriend}</p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12 angel-friend-heading-3">
+                                <p className="no-margin">THE</p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12 angel-friend-heading-4">
+                                <p className="no-margin">UNCOOLFRIEND</p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12 angel-friend-heading-3">
+                                <p className="no-margin">FOR TONIGHT!</p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12 angel-friend-heading-5">
+                                <p className="with-margin">Drive safe, do not drink and drive,<br />and don't try to impress anyone<br />with dangerous driving</p>
+                            </div>
+                        </div>
+                        <div className="row with-margin">
+                            <div className="col-12">
+                                <button 
+                                    id="get-prize" 
+                                    className="btn btn-primary get-prize-btn w-100"
+                                    onClick={getPrize}
+                                >
+                                    Get your free #Uncool gift
+                                </button>
+                            </div>
+                        </div>
+                        <div className="row with-margin">
+                            <div className="col-6">
+                                <button className="btn btn-primary stay-btn w-100">
+                                    #StayUncool
+                                </button>
+                            </div>
+                            <div className="col-6">
+                                <button className="btn btn-primary stay-btn w-100">
+                                    #StaySafe
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+            {showPrize && (
+                <div className="row prize rounded-top text-center">
+                    <div className="col-12">
+                        <div className="row">
+                            <div className="col-12 send-email-heading">
+                                <p>Send your email address to receive<br />a hoodie created by ZeroNero<br />of their new #Uncool Collection</p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12" id="uncool-friend-div">
+                                <input type="text" placeholder="name@emailaddress.com" className="form-control email-input" />
+                            </div>
+                        </div>
+                        <div className="row with-margin">
+                            <div className="col-12">
+                                <button 
+                                    id="send" 
+                                    className="btn btn-primary send-btn"
+                                    onClick={sendEmailAddress}
+                                >
+                                    SEND
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             )}
